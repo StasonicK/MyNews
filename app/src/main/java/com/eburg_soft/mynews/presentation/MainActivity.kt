8e26e97ad843f0813f39df6ce13dfd5631474838
Stas Anishchenko <anischenko_stas@mail.ru>
2020-11-04 12:37:37 +0500

    - created packages;
    - filled gradle files

---
+++ app/src/main/java/com/eburg_soft/mynews/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.eburg_soft.mynews.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eburg_soft.mynews.R.layout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+    }
+}
