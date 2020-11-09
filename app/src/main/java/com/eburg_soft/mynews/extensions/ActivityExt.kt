8b9da359b0f7a7b10acf85f72c75ec53d61cbd53
Stas Anishchenko <anischenko_stas@mail.ru>
2020-11-09 14:59:36 +0500

    - finished

--- app/src/main/java/com/eburg_soft/mynews/extensions/ActivityExt.kt
+++
@@ -1,7 +0,0 @@
-package com.eburg_soft.mynews.extensions
-
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-
-val FragmentManager.currentNavigationFragment: Fragment?
-    get() = primaryNavigationFragment?.childFragmentManager?.fragments?.first()
